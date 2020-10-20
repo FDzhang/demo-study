5307cad54b8fce04ae29b5c6595811b33ab5a10f
zhangxinqiang <1933581361@qq.com>
2020-10-20 17:26:25 +0800

    demo-mysql

---
+++ demo-springboot/demo-mysql/src/main/java/com/example/demomysql/controller/TestController.java
@@ -0,0 +1,27 @@
+package com.example.demomysql.controller;
+
+import com.example.demomysql.mapper.TestMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ：zxq
+ * @date ：Created in 2020/10/20 11:01
+ */
+@RestController
+public class TestController {
+
+    @Autowired
+    private TestMapper testMapper;
+
+    @GetMapping("test")
+    public String test(){
+        return "hello world";
+    }
+
+    @GetMapping("count")
+    public int test2(){
+        return testMapper.count();
+    }
+}
---
+++ demo-springboot/demo-mysql/src/main/java/com/example/demomysql/mapper/TestMapper.java
@@ -0,0 +1,15 @@
+package com.example.demomysql.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+/**
+ * @author ：zxq
+ * @date ：Created in 2020/10/20 11:03
+ */
+@Mapper
+public interface TestMapper {
+
+    @Select("select count(*) from t_user")
+    public int count();
+}
