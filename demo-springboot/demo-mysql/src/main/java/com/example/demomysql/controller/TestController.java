ceff2de0802be34b0c93daa2dac260e35a0c71a7
zhangxinqiang <1933581361@qq.com>
2020-10-21 16:26:10 +0800

    demo-mysql: add post test

--- demo-springboot/demo-mysql/src/main/java/com/example/demomysql/controller/TestController.java
+++ demo-springboot/demo-mysql/src/main/java/com/example/demomysql/controller/TestController.java
@@ -3,7 +3,10 @@ package com.example.demomysql.controller;
 import com.example.demomysql.mapper.TestMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author ：zxq
@@ -23,5 +26,12 @@ public class TestController {
     @GetMapping("count")
     public int test2(){
         return testMapper.count();
+    }
+
+    @PostMapping("test")
+    public String test3(HttpServletRequest request){
+        System.out.println(request.getParameter("str"));
+        System.out.println(request.getParameter("id"));
+        return "successs";
     }
 }
