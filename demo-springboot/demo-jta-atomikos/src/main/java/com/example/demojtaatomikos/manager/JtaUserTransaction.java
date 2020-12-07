03ef965b63b6895fa6650d398dd7b26585c51cc0
zhangxinqiang <1933581361@qq.com>
2020-12-07 18:10:36 +0800

    demo-jta-atomikos

--- demo-springboot/demo-jta-atomikos/src/main/java/com/example/demojtaatomikos/manager/JtaUserTransaction.java
+++ demo-springboot/demo-jta-atomikos/src/main/java/com/example/demojtaatomikos/manager/JtaUserTransaction.java
@@ -4,19 +4,17 @@ import com.example.demojtaatomikos.util.MongoUtils;
 import com.mongodb.TransactionOptions;
 import com.mongodb.client.ClientSession;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.mongodb.MongoDatabaseFactory;
-import org.springframework.data.mongodb.MongoDbFactory;
-import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.data.mongodb.*;
 import org.springframework.lang.Nullable;
 import org.springframework.transaction.TransactionDefinition;
-import org.springframework.transaction.support.ResourceHolderSupport;
-import org.springframework.transaction.support.SmartTransactionObject;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
-import org.springframework.transaction.support.TransactionSynchronizationUtils;
+import org.springframework.transaction.TransactionSystemException;
+import org.springframework.transaction.support.*;
 import org.springframework.util.Assert;
+import org.springframework.util.ClassUtils;
 
 import javax.transaction.*;
 import java.io.Serializable;
+import java.lang.reflect.Method;
 import java.util.Objects;
 
 /**
@@ -31,7 +29,7 @@ public class JtaUserTransaction implements UserTransaction, Serializable {
     private transient MongoTransactionManager mongoTransactionManager;
     private final transient MongoUtils mongoUtils;
 
-    public JtaUserTransaction(MongoTransactionManager mongoTransactionManager, MongoUtils mongoUtils){
+    public JtaUserTransaction(MongoTransactionManager mongoTransactionManager, MongoUtils mongoUtils) {
         this.mongoTransactionManager = mongoTransactionManager;
         this.mongoUtils = mongoUtils;
     }
@@ -44,10 +42,93 @@ public class JtaUserTransaction implements UserTransaction, Serializable {
         mongoUtils.setSessionSynchronizationForTransactionBegin();
     }
 
+    private static MongoTransactionObject extractMongoTransaction(Object transaction) {
+        Assert.isInstanceOf(MongoTransactionObject.class, transaction,
+                () -> String.format("Expected to find a %s but it turned out to be %s.", MongoTransactionObject.class,
+                        transaction.getClass()));
+        return (MongoTransactionObject) transaction;
+    }
+
+    private Object getMongoTransaction() {
+        MongoDatabaseFactory mongoDatabaseFactory = mongoTransactionManager.getDbFactory();
+        Assert.notNull(mongoDatabaseFactory, "Db factory must not be null");
+        MongoResourceHolder resourceHolder = (MongoResourceHolder) TransactionSynchronizationManager.getResource(mongoDatabaseFactory);
+        return new MongoTransactionObject(resourceHolder);
+    }
+
+    private MongoResourceHolder newResourceHolder(TransactionDefinition definition) {
+        MongoDatabaseFactory dbFactory = mongoTransactionManager.getDbFactory();
+        Class<MongoDatabaseUtils> mongoDatabaseUtilsClass = MongoDatabaseUtils.class;
+        ClientSession session = null;
+        try {
+            Method doGetSession = mongoDatabaseUtilsClass.getDeclaredMethod(
+                    "doGetSession", MongoDatabaseFactory.class, SessionSynchronization.class);
+            doGetSession.setAccessible(true);
+            session = (ClientSession) doGetSession.invoke(
+                    mongoDatabaseUtilsClass.newInstance(), dbFactory, SessionSynchronization.ALWAYS
+            );
+        } catch (Exception e) {
+            log.error("getSession srr;", e.getCause());
+        }
+        if (Objects.nonNull(session)) {
+            MongoResourceHolder resourceHolder = new MongoResourceHolder(session, dbFactory);
+            resourceHolder.setTimeoutIfNotDefaulted(determineTimeout(definition));
+            return resourceHolder;
+        }
+        return null;
+    }
+
+    private int determineTimeout(TransactionDefinition definition) {
+        if (definition.getTimeout() != TransactionDefinition.TIMEOUT_DEFAULT) {
+            return definition.getTimeout();
+        }
+        return TransactionDefinition.TIMEOUT_DEFAULT;
+    }
+
+    private static String debugString(@Nullable ClientSession session) {
+        if (session == null) {
+            return "null";
+        }
+
+        String debugString = String.format("[%s@%s ", ClassUtils.getShortName(session.getClass()),
+                Integer.toHexString(session.hashCode()));
+        try {
+            if (session.getServerSession() != null) {
+                debugString += String.format("id = %s, ", session.getServerSession().getIdentifier());
+                debugString += String.format("causallyConsistent = %s, ", session.isCausallyConsistent());
+                debugString += String.format("txActive = %s, ", session.hasActiveTransaction());
+                debugString += String.format("txNumber = %d, ", session.getServerSession().getTransactionNumber());
+                debugString += String.format("closed = %s, ", session.getServerSession().isClosed());
+                debugString += String.format("clusterTime = %s", session.getClusterTime());
+            } else {
+                debugString += "id = n/a";
+                debugString += String.format("causallyConsistent = %s, ", session.isCausallyConsistent());
+                debugString += String.format("txActive = %s, ", session.hasActiveTransaction());
+                debugString += String.format("clusterTime = %s", session.getClusterTime());
+            }
+        } catch (Exception e) {
+            debugString += String.format("error = %s", e.getMessage());
+        }
+        debugString += "]";
+        return debugString;
+    }
+
     @Override
     public void commit() throws RollbackException, HeuristicMixedException, HeuristicRollbackException, SecurityException, IllegalStateException, SystemException {
-        Assert.notNull(mongoTransactionManager.getDbFactory(),"DbFactory must not be null!");
-        if (Objects.nonNull(TransactionSynchronizationManager.getResource(mongoTransactionManager.getDbFactory()))){
+        Assert.notNull(mongoTransactionManager.getDbFactory(), "DbFactory must not be null!");
+        if (Objects.nonNull(TransactionSynchronizationManager.getResource(mongoTransactionManager.getDbFactory()))) {
+            MongoTransactionObject mongoTransactionObject = extractMongoTransaction(getMongoTransaction());
+            MongoResourceHolder resourceHolder = newResourceHolder(new DefaultTransactionDefinition());
+            mongoTransactionObject.setResourceHolder(resourceHolder);
+
+            try {
+                mongoTransactionObject.commitTransaction();
+                TransactionSynchronizationManager.unbindResource(mongoTransactionManager.getDbFactory());
+                mongoTransactionObject.getRequiredResourceHolder().clear();
+                mongoTransactionObject.closeSession();
+                mongoUtils.setSessionSynchronizationForTransactionCompletion();
+            } catch (Exception ex) {
+            }
         }
     }
 
@@ -71,22 +152,23 @@ public class JtaUserTransaction implements UserTransaction, Serializable {
 
     }
 
-    public static String s(){
+    public static String s() {
         return null;
     }
 
 
-
-    protected static class MongoTransactionObject implements SmartTransactionObject{
+    protected static class MongoTransactionObject implements SmartTransactionObject {
         @Nullable
         private MongoResourceHolder resourceHolder;
 
-        MongoTransactionObject(@Nullable MongoResourceHolder resourceHolder){
+        MongoTransactionObject(@Nullable MongoResourceHolder resourceHolder) {
             this.resourceHolder = resourceHolder;
         }
+
         void setResourceHolder(@Nullable MongoResourceHolder resourceHolder) {
             this.resourceHolder = resourceHolder;
         }
+
         final boolean hasResourceHolder() {
             return resourceHolder != null;
         }
@@ -96,6 +178,7 @@ public class JtaUserTransaction implements UserTransaction, Serializable {
         public ClientSession getSession() {
             return resourceHolder != null ? resourceHolder.getSession() : null;
         }
+
         private ClientSession getRequiredSession() {
             ClientSession session = getSession();
             Assert.state(session != null, "A Session is required but it turned out to be null.");
@@ -103,12 +186,14 @@ public class JtaUserTransaction implements UserTransaction, Serializable {
         }
 
 
-        void startTransaction(@Nullable TransactionOptions options){
+        void startTransaction(@Nullable TransactionOptions options) {
             ClientSession session = getRequiredSession();
         }
+
         public void commitTransaction() {
             getRequiredSession().commitTransaction();
         }
+
         public void abortTransaction() {
             getRequiredSession().abortTransaction();
         }
@@ -136,11 +221,12 @@ public class JtaUserTransaction implements UserTransaction, Serializable {
         }
     }
 
-    class MongoResourceHolder extends ResourceHolderSupport{
-        private @Nullable ClientSession session;
+    class MongoResourceHolder extends ResourceHolderSupport {
+        private @Nullable
+        ClientSession session;
         private MongoDatabaseFactory dbFactory;
 
-        MongoResourceHolder(@Nullable ClientSession session, MongoDatabaseFactory dbFactory){
+        MongoResourceHolder(@Nullable ClientSession session, MongoDatabaseFactory dbFactory) {
             this.session = session;
             this.dbFactory = dbFactory;
         }
