3b653bb565f4dcf875106c60a1697c2e8b79f431
zhangxinqiang <1933581361@qq.com>
2021-01-14 16:13:57 +0800

    springboot web flux demo sse Test

--- demo-springboot/demo-webflux/src/main/java/com/example/demowebflux/controller/CityController.java
+++ demo-springboot/demo-webflux/src/main/java/com/example/demowebflux/controller/CityController.java
@@ -4,9 +4,13 @@ import com.example.demowebflux.bean.City;
 import com.example.demowebflux.handler.CityHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
 /**
  * web flux city controller
@@ -21,6 +25,24 @@ public class CityController {
 
     @Autowired
     private CityHandler cityHandler;
+
+    /**
+     * Flux : 返回0-n个元素
+     * 注：需要指定MediaType
+     * @return
+     */
+    @GetMapping(value = "/sse", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    private Flux<String> flux() {
+        Flux<String> result = Flux
+                .fromStream(IntStream.range(1, 5).mapToObj(i -> {
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                    } catch (InterruptedException e) {
+                    }
+                    return "flux data--" + i;
+                }));
+        return result;
+    }
 
     @GetMapping("/{id}")
     public Mono<City> findCityById(@PathVariable("id") Long id) {
