7c21819cc729016672277b6829a095bfe955283b
zhangxinqiang <1933581361@qq.com>
2021-03-02 23:40:17 +0800

    demo-mdc 1.Springboot 日志的使用

--- demo-springboot/demo-mdc/src/main/java/com/example/demomdc/controller/WelcomeController.java
+++ demo-springboot/demo-mdc/src/main/java/com/example/demomdc/controller/WelcomeController.java
@@ -15,4 +15,17 @@ public class WelcomeController {
         log.info("inside the welcomeMessage");
         return "Welcome to http://www.SpringBootDev.com";
     }
+
+    @GetMapping("level")
+    public void level(){
+        /**
+         * 通过 Logger 的api打印信息
+         * 日志的级别；
+         * 由低到高   trace<debug<info<warn<error
+         */
+        log.debug("这是日志");
+        log.info("这是日志");
+        log.warn("这是日志");
+        log.error("这是日志");
+    }
 }
