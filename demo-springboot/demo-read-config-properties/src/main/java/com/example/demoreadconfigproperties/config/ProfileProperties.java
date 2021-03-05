cfb634ae4d07df90f0dbdd6f8cc0b78139723c2d
zhangxinqiang <1933581361@qq.com>
2021-03-05 17:54:40 +0800

    update

--- demo-springboot/demo-read-config-properties/src/main/java/com/example/demoreadconfigproperties/config/ProfileProperties.java
+++ demo-springboot/demo-read-config-properties/src/main/java/com/example/demoreadconfigproperties/config/ProfileProperties.java
@@ -8,6 +8,7 @@ import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import java.util.Arrays;
 
 @Getter
 @Setter
@@ -23,4 +24,18 @@ public class ProfileProperties {
     private String email;
 
     private Boolean handsome = Boolean.TRUE;
+
+    public static void main(String[] args) {
+        int capacity = 3;
+        int[] x = new int[capacity];
+        for (int i = 0; i < x.length; i++) {
+            x[i]=i;
+        }
+        int newCapacity = capacity * 2;
+        x = Arrays.copyOf(x, newCapacity);
+        capacity = newCapacity;
+
+        System.out.println(Arrays.toString(x));
+        System.out.println(capacity);
+    }
 }
