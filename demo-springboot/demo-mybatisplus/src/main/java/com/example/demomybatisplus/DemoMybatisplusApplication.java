f7759c940e38407df768ab60a9ea7c6b8716d1c5
zhangxinqiang <1933581361@qq.com>
2021-04-20 23:18:01 +0800

    demo-mybatisplus 1.分页条件查询

--- demo-springboot/demo-mybatisplus/src/main/java/com/example/demomybatisplus/DemoMybatisplusApplication.java
+++ demo-springboot/demo-mybatisplus/src/main/java/com/example/demomybatisplus/DemoMybatisplusApplication.java
@@ -1,9 +1,11 @@
 package com.example.demomybatisplus;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.demomybatisplus.mapper")
 public class DemoMybatisplusApplication {
 
     public static void main(String[] args) {
