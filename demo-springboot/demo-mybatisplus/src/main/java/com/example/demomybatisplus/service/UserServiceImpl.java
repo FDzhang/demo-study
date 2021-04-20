e3f0533aca387c039f911c334b066deb5614010b
zhangxinqiang <1933581361@qq.com>
2021-04-20 23:42:21 +0800

    demo-mybatisplus 1.mapper->service->controller

---
+++ demo-springboot/demo-mybatisplus/src/main/java/com/example/demomybatisplus/service/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package com.example.demomybatisplus.service;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.example.demomybatisplus.bean.User;
+import com.example.demomybatisplus.mapper.UserMapper;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author : zxq
+ * @date-time : 2021/4/20 23:35
+ */
+
+@Service
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> {
+}
