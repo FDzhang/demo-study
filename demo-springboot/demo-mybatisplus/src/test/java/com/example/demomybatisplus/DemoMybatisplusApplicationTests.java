3ffcd1ae563488739ac9037f35d0739560695d22
zhangxinqiang <1933581361@qq.com>
2021-04-21 23:30:27 +0800

    demo-mybatisplus 1.指定查询字段 （select）

---
+++ demo-springboot/demo-mybatisplus/src/test/java/com/example/demomybatisplus/DemoMybatisplusApplicationTests.java
@@ -0,0 +1,38 @@
+package com.example.demomybatisplus;
+
+import com.alibaba.fastjson.JSON;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.example.demomybatisplus.bean.User;
+import com.example.demomybatisplus.mapper.UserMapper;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@SpringBootTest
+class DemoMybatisplusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Resource
+    private UserMapper mapper;
+
+    @Test
+    void test(){
+        PageHelper.startPage(1,2);
+        QueryWrapper<User> queryWrapper=  new QueryWrapper<>();
+        queryWrapper.select("name");
+        queryWrapper.eq("name","test");
+        queryWrapper.orderByDesc("age");
+        List<User> users = mapper.selectList(queryWrapper);
+        PageInfo<User> pageInfo = new PageInfo<>(users);
+
+        System.out.println(JSON.toJSONString(pageInfo));
+    }
+}
