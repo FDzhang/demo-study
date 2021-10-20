dca405e5f79b350f34ffd3150a5b1c06c52b7c1d
zhangxinqiang <1933581361@qq.com>
2021-10-20 18:53:51 +0800

    swagger 注解eg

--- demo-springboot/demo-swagger/src/main/java/com/example/demoswaggerui/controller/TestController.java
+++ demo-springboot/demo-swagger/src/main/java/com/example/demoswaggerui/controller/TestController.java
@@ -1,13 +1,8 @@
 package com.example.demoswaggerui.controller;
 
 import com.example.demoswaggerui.model.User;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.*;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author ：zxq
@@ -17,21 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping
 //Api注解，描述信息 可通过tag进行分类
-@Api(value = "TestController")
+@Api(tags = "TestController")
 public class TestController {
 
-//    //方法描述
-//    @ApiOperation(notes = "get方法", value = "test3")
-//    @GetMapping("/test")
-//    public String test1(@ApiParam(name = "test", value = "测试") String test,
-//                        @ApiParam(name = "id", value = "id") Integer id) {
-//        return "success:" + test + id;
-//    }
-
     //方法描述
-    @ApiOperation(notes = "post方法", value = "putUser")
-    @PostMapping("/putUser")
-    public User test9(@ApiParam(name = "User", value = "用户") User user) {
+    @ApiOperation(notes = "post方法", value = "put_user")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "ok", response = User.class)
+    })
+    @PostMapping("/put_user")
+    public User putUser(@RequestBody User user) {
         System.out.println(user);
         return user;
     }
--- demo-springboot/demo-swagger/src/main/java/com/example/demoswaggerui/model/User.java
+++ demo-springboot/demo-swagger/src/main/java/com/example/demoswaggerui/model/User.java
@@ -10,11 +10,14 @@ import lombok.Data;
  */
 
 @Data
-@ApiModel
+@ApiModel("用户类")
 public class User {
     // 属性描述
     @ApiModelProperty(value = "名字")
     private String name;
     @ApiModelProperty(value = "年龄")
     private Integer age;
+    // hidden 隐藏
+    @ApiModelProperty(value = "体重", hidden = true)
+    private Integer weight;
 }
