79cdf9aaf6d4c437d836e36168ee93f5dd60d34f
zhangxinqiang <1933581361@qq.com>
2022-03-02 22:27:49 +0800

    spring-cloud feign

---
+++ spring-cloud/feign/feign-demo01-provider/src/main/java/cn/seasun/feignproviderdemo/DemoProviderApplication.java
@@ -0,0 +1,17 @@
+package cn.seasun.feignproviderdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : zxq
+ * @create : 2022/3/2 21:49
+ */
+@SpringBootApplication
+public class DemoProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoProviderApplication.class, args);
+    }
+
+}
