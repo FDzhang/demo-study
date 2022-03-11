29d493f89deb56a08163252232278afd2e57bb9d
zhangxinqiang <1933581361@qq.com>
2022-03-11 21:59:19 +0800

    spring-cloud nacos config

--- spring-security/springsecurity-demo-role/src/main/java/cn/seasun/springsecuritydemo/config/SecurityConfig.java
+++ spring-security/springsecurity-demo-role/src/main/java/cn/seasun/springsecuritydemo/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 /**
@@ -13,6 +14,31 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
  *
  * 创建 SecurityConfig 配置类，继承 WebSecurityConfigurerAdapter 抽象类，实现 Spring Security 在 Web 场景下的自定义配置。代码如下：
  * 我们可以通过重写 WebSecurityConfigurerAdapter 的方法，实现自定义的 Spring Security 的配置。
+ *
+ *
+ * <X> 处，调用 HttpSecurity#authorizeRequests() 方法，开始配置 URL 的权限控制。注意看艿艿配置的四个权限控制的配置。下面，是配置权限控制会使用到的方法：
+ *
+ * #(String... antPatterns) 方法，配置匹配的 URL 地址，基于 Ant 风格路径表达式 ，可传入多个。
+ * 【常用】#permitAll() 方法，所有用户可访问。
+ * 【常用】#denyAll() 方法，所有用户不可访问。
+ * 【常用】#authenticated() 方法，登录用户可访问。
+ * #anonymous() 方法，无需登录，即匿名用户可访问。
+ * #rememberMe() 方法，通过 remember me 登录的用户可访问。
+ * #fullyAuthenticated() 方法，非 remember me 登录的用户可访问。
+ * #hasIpAddress(String ipaddressExpression) 方法，来自指定 IP 表达式的用户可访问。
+ * 【常用】#hasRole(String role) 方法， 拥有指定角色的用户可访问。
+ * 【常用】#hasAnyRole(String... roles) 方法，拥有指定任一角色的用户可访问。
+ * 【常用】#hasAuthority(String authority) 方法，拥有指定权限(authority)的用户可访问。
+ * 【常用】#hasAuthority(String... authorities) 方法，拥有指定任一权限(authority)的用户可访问。
+ * 【最牛】#access(String attribute) 方法，当 Spring EL 表达式的执行结果为 true 时，可以访问。
+ * <Y> 处，调用 HttpSecurity#formLogin() 方法，设置 Form 表单登录。
+ *
+ * 如果胖友想要自定义登录页面，可以通过 #loginPage(String loginPage) 方法，来进行设置。不过这里我们希望像「2. 快速入门」一样，使用默认的登录界面，所以不进行设置。
+ * <Z> 处，调用 HttpSecurity#logout() 方法，配置退出相关。
+ * 如果胖友想要自定义退出页面，可以通过 #logoutUrl(String logoutUrl) 方法，来进行设置。不过这里我们希望像「2. 快速入门」一样，使用默认的退出界面，所以不进行设置。
+ *
+ *
+ * 修改 SecurityConfig 配置类，增加 @EnableGlobalMethodSecurity 注解，开启对 Spring Security 注解的方法，进行权限验证。代码如下：
  *
  */
 @Configuration
---
+++ spring-security/springsecurity-demo-role/src/main/java/cn/seasun/springsecuritydemo/controller/DemoController.java
@@ -0,0 +1,51 @@
+package cn.seasun.springsecuritydemo.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.security.PermitAll;
+
+/**
+ * 每个 URL 的权限验证，和「3.2.2 TestController」是一一对应的。
+ * @PermitAll 注解，等价于 #permitAll() 方法，所有用户可访问。
+ *
+ * 重要！！！因为在「3.2.1 SecurityConfig」中，配置了 .anyRequest().authenticated() ，任何请求，访问的用户都需要经过认证。所以这里 @PermitAll 注解实际是不生效的。
+ *
+ * 也就是说，Java Config 配置的权限，和注解配置的权限，两者是叠加的。
+ *
+ * @PreAuthorize 注解，等价于 #access(String attribute) 方法，，当 Spring EL 表达式的执行结果为 true 时，可以访问。
+ *
+ * Spring Security 还有其它注解，不过不太常用，可见《区别： @Secured(), @PreAuthorize() 及 @RolesAllowed()》文章。
+ *
+ * 胖友可以按照如上的说明，进行各种测试。例如说，登录「normal/normal」用户后，去访问 /test/admin 接口，会返回 403 界面，无权限~
+ */
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @PermitAll
+    @GetMapping("/echo")
+    public String demo() {
+        return "示例返回";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "我是首页";
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/admin")
+    public String admin() {
+        return "我是管理员";
+    }
+
+    @PreAuthorize("hasRole('ROLE_NORMAL')")
+    @GetMapping("/normal")
+    public String normal() {
+        return "我是普通用户";
+    }
+
+}
