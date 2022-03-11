88a1f5998a2261a8074d73c504d7b64b5b0a4557
zhangxinqiang <1933581361@qq.com>
2022-03-11 18:36:13 +0800

    spring-security

---
+++ spring-security/springsecurity-demo/src/main/java/cn/seasun/springsecuritydemo/Application.java
@@ -0,0 +1,18 @@
+package cn.seasun.springsecuritydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangxinqiang
+ * @create 2022/3/11 18:05
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
