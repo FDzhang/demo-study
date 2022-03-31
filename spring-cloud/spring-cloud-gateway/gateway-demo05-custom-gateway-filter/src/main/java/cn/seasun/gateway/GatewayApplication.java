e0da63ec47fe49d8b9f3483db368fb2e5b2662a0
zhangxinqiang <1933581361@qq.com>
2022-03-31 15:30:35 +0800

    spring-cloud gateway Filter 认证

---
+++ spring-cloud/spring-cloud-gateway/gateway-demo05-custom-gateway-filter/src/main/java/cn/seasun/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package cn.seasun.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangxinqiang
+ * @create 2022/3/29 16:39
+ * <p>
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
