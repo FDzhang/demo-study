33ac4b69a6434cd39626dad4c43be4c770978275
zhangxinqiang <1933581361@qq.com>
2022-03-31 14:30:23 +0800

    spring-cloud gateway 基于配置中心 Nacos 实现动态路由

---
+++ spring-cloud/spring-cloud-gateway/gateway-demo03-config-nacos/src/main/java/cn/seasun/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package cn.seasun.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangxinqiang
+ * @create 2022/3/29 16:39
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
--- spring-cloud/spring-cloud-gateway/user-service/src/main/java/cn/seasun/userservicedemo/controller/UserController.java
+++ spring-cloud/spring-cloud-gateway/user-service/src/main/java/cn/seasun/userservicedemo/controller/UserController.java
@@ -14,6 +14,7 @@ public class UserController {
 
     @GetMapping("/get")
     public UserDTO get(@RequestParam("id") Integer id) {
+        System.err.println("ccurrent " + System.currentTimeMillis() / 1000);
         return new UserDTO().setId(id)
                 .setName("没有昵称：" + id)
                 .setGender(id % 2 + 1); // 1 - 男；2 - 女
@@ -21,6 +22,7 @@ public class UserController {
 
     @PostMapping("/add")
     public Integer add(UserAddDTO addDTO) {
+        System.err.println("ccurrent " + System.currentTimeMillis() / 1000);
         return (int) (System.currentTimeMillis() / 1000); // 嘿嘿，随便返回一个 id
     }
 
